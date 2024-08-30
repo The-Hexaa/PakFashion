0b95dc0dcd17d60c2485ee6785850c54e4c962e9
Dilawaiz Khan <dilawaizkhan08@gmail.com>
2024-08-30 15:07:36 +0500

    prompt updated

--- src/urls_finder.py
+++ src/urls_finder.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
-# from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
 import time
 import re
 from urllib.parse import urlparse
